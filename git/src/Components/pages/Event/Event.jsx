f0455c3024b6b4b71585495b99f729406d4ea7da
SelnoxInfotech <117904460+SelnoxInfotech@users.noreply.github.com>
2023-02-03 10:22:56 +0530

    Delete git directory

--- git/src/Components/pages/Event/Event.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Event() {
-  return (
-    <div className='container center page'>Event</div>
-  )
-}
--- git/src/Components/pages/Schedule/Schedule.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Schedule() {
-  return (
-    <div className='container center page'>Schedule</div>
-  )
-}
